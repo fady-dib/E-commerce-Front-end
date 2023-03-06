182c51f90d15afa6d48a99ac1b700e35c2ecbcad
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-03-06 15:54:36 +0200

    added path for burger menu

---
+++ script.js
@@ -0,0 +1,15 @@
+  let user=document.getElementById("user")
+  let sign_up=document.getElementById("sign_up")
+  let admin=document.getElementById("admin")
+
+  user.addEventListener('click',()=>{
+    window.location.href = "pages/login.html";
+  });
+
+  sign_up.addEventListener('click',()=>{
+    window.location.href = "pages/signup.html";
+  });
+
+  admin.addEventListener('click',()=>{
+    window.location.href = "pages/login.html";
+  });
